d831a1ec7e311b3ddc8032315d415f4a7ec848ea
Eddie Moon <eddiemoon@eddies-mbp.home>
2022-09-29 23:06:17 -0400

    fix: favicon

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // create axios instance
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://idea-tank.herokuapp.com/' });
 
 // middleware send auth user to backend
 API.interceptors.request.use((req) => {
